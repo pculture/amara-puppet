a32a90d4280b6aaae7ebe2e251a95c7a0595247b
Evan Hazlett <ejhazlett@gmail.com>
2012-07-06 22:09:30 -0400

    updated nginx module

--- puppetmaster/modules/nginx/manifests/config.pp
+++ puppetmaster/modules/nginx/manifests/config.pp
@@ -4,12 +4,16 @@ class nginx::config inherits nginx::params {
     path      => "${::path}",
     logoutput => on_failure,
   }
+  $nginx_pkg = $::operatingsystemrelease ? {
+    '10.04': Package['nginx'],
+    default: Package['nginx-full'],
+  }
   file { 'nginx::config::vhost_unisubs_example_com':
     path    => '/etc/nginx/conf.d/unisubs.example.com.conf',
     content => template('nginx/vhost_unisubs.example.com.conf.erb'),
     owner   => "${nginx::config::www_user}",
     mode    => 0644,
-    require => Package['nginx-full'],
+    require => $nginx_pkg,
     notify  => Service['nginx'],
   }
 }
