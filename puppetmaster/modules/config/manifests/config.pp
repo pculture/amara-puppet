92ba06a1af8fc1216981eb7f20e2bdddb7727d69
Evan Hazlett <ejhazlett@gmail.com>
2012-07-09 12:12:30 -0400

    removed deprecated dynamic variable lookup

--- puppetmaster/modules/config/manifests/config.pp
+++ puppetmaster/modules/config/manifests/config.pp
@@ -1,6 +1,11 @@
 class config::config inherits config::params {
   $project_dir = "${appserver::app_dir}/unisubs"
-
+  # this is duplicated from config/manifests/init.pp due to the deprecated 
+  # dynamic lookup in puppet 2.7 (http://docs.puppetlabs.com/guides/scope_and_puppet.html)
+  $roles = $::system_roles ? {
+    undef => [],
+    default => $::system_roles,
+  }
   if ($::virtual == 'virtualbox') {
     user { 'vagrant':
       ensure  => present,
@@ -17,7 +22,7 @@ class config::config inherits config::params {
   }
   # custom role configs
   # app role
-  if 'app' in $config::roles {
+  if 'app' in $config::config::roles {
     require closure
     # unisubs closure library link
     file { 'config::config::unisubs_closure_library_link':
