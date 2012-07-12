d4c39132b6a945d9229155e822b499d907ff5d3a
Evan Hazlett <ejhazlett@gmail.com>
2012-07-11 23:29:54 -0400

    updated node bootstrap script

--- nodes/bootstrap.sh
+++ nodes/bootstrap.sh
@@ -9,6 +9,9 @@ echo "- role: `hostname -s`\n" > /etc/system_roles.yml
 # update apt
 apt-get update 2>&1 > /dev/null
 
+# create the initial puppet.conf ; needed to sync puppet role facts
+mkdir -p /etc/puppet
+echo "[main]\n  pluginsync = true\n" > /etc/puppet/puppet.conf
 # run the initial sync in the foreground
 puppet agent -t
 
