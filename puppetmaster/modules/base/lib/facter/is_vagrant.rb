0e020cf7b41e196c0459e07cf3284c6befc56a4c
Evan Hazlett <ejhazlett@gmail.com>
2012-08-10 13:12:03 -0500

    updated is_vagrant fact

--- puppetmaster/modules/base/lib/facter/is_vagrant.rb
+++ puppetmaster/modules/base/lib/facter/is_vagrant.rb
@@ -1,10 +1,6 @@
 Facter.add("is_vagrant") do
   confine :kernel => "Linux"
   setcode do
-    is_vagrant = false
-    if File.exist? "/home/vagrant"
-      is_vagrant = true
-    end
-    is_vagrant
+    File.exist? "/home/vagrant"
   end
 end
