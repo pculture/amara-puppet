6384b957d17d21bf44dbdced518cb5e1176ffe8e
Evan Hazlett <ejhazlett@gmail.com>
2012-08-10 13:28:18 -0500

    updated data role

--- puppetmaster/modules/config/manifests/roles/data.pp
+++ puppetmaster/modules/config/manifests/roles/data.pp
@@ -17,7 +17,7 @@ class config::roles::data {
   if ! defined(Package['libmysqlclient-dev']) { package { 'libmysqlclient-dev': ensure => installed, } }
 
   # if running in the local test vagrant multi-vm, include mysql for local testing
-  if ($::is_vagrant) {
+  if ('vagrant' in $config::roles) {
     if ! defined(Class['mysql']) { class { 'mysql': } }
   }
 
