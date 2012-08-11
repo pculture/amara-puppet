495cbc821a0159633a8ad1be3829f5d6c37622e6
Evan Hazlett <ejhazlett@gmail.com>
2012-08-11 03:03:34 -0400

    updated appserver module

--- puppetmaster/modules/appserver/manifests/package.pp
+++ puppetmaster/modules/appserver/manifests/package.pp
@@ -1,3 +1,4 @@
 class appserver::package {
+  if ! defined(Package['openjdk-6-jre']) { package { 'openjdk-6-jre': ensure => installed, } }
   if ! defined(Package['mysql-client']) { package { 'mysql-client': ensure  => present,} }
 }
--- puppetmaster/modules/config/manifests/package.pp
+++ puppetmaster/modules/config/manifests/package.pp
@@ -1,3 +1,2 @@
 class config::package {
-  if ! defined(Package["openjdk-6-jre"]) { package { "openjdk-6-jre": ensure => installed, } }
 }
