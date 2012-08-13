1877312e0566b66f60d9813d6d75809cadc7c5d3
Evan Hazlett <ejhazlett@gmail.com>
2012-08-12 21:05:48 -0400

    updated custom facts

--- puppetmaster/modules/base/lib/facter/system_env.rb
+++ puppetmaster/modules/base/lib/facter/system_env.rb
@@ -1,7 +1,7 @@
 require 'yaml'
 Facter.add("system_environments") do
   setcode do
-   all_environments = Array.new
+    all_environments = Array.new
     begin
       cfg = File::open('/etc/system_environments.yml')
       environments = YAML::load(cfg.read())
